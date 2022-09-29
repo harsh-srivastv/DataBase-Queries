db82508b85e9589c8836a12e4f2f7d67697131f9
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-09-29 15:11:56 +0530

    Delete 01_2nd_MAX.sql

--- 01_2nd_MAX.sql
+++
@@ -1,8 +0,0 @@
-
--- Quesion: Select the second maximum salary from the table
-
--- Method 1
-Select max(sal) from emp where sal not in(Select max(sal) from emp);
-
--- Method 2
-Select max(sal) from emp where sal < (Select max(sal) from emp);
